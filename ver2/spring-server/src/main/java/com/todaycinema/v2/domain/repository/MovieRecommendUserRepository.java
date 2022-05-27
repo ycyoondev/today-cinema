0643e12d6e8755a9e7c2ebbd88a3c43a9e6869a6
YC Yoon <z20152002@gmail.com>
2022-05-27 23:51:51 +0900

    Dev: Record recommended movies to user

---
+++ ver2/spring-server/src/main/java/com/todaycinema/v2/domain/repository/MovieRecommendUserRepository.java
@@ -0,0 +1,4 @@
+package com.todaycinema.v2.domain.repository;
+
+public interface MovieRecommendUserRepository extends org.springframework.data.jpa.repository.JpaRepository<com.todaycinema.v2.domain.MovieRecommendUser, java.lang.Long> {
+}
