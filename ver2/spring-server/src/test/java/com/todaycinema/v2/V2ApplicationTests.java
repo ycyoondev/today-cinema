dbb10455d8c73e30a227db380c99c3398bb5a7d3
YC Yoon <z20152002@gmail.com>
2022-06-15 23:51:56 +0900

    feat(Spring Sever): Test code environment settings
    테스트 코드 환경설정
    - gitignore 해제
    - WebClient Test 작성
    - DB update Test 작성

---
+++ ver2/spring-server/src/test/java/com/todaycinema/v2/V2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todaycinema.v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ver2/spring-server/src/test/java/com/todaycinema/v2/apitest/WebClientTest.java
@@ -0,0 +1,20 @@
+package com.todaycinema.v2.apitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+public class WebClientTest {
+
+    @Test
+    void doTest() {
+        WebClient client = WebClient.create();
+        String url = "https://jsonplaceholder.typicode.com/todos/1";
+        Mono<String> stringMono = client.get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(String.class);
+        System.out.println(stringMono.flux().toStream().findFirst());
+        System.out.println("종료");
+    }
+}
